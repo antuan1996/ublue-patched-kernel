81c8387ae22bf58a3da0f703492174716b424022
antuan1996 <antuan1996@users.noreply.github.com>
2023-09-17 17:52:23 +0300

    Initial commit

---
+++ config/scripts/cachyos_kernel_override.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo 'CachyOS kernel override'
+rpm-ostree cliwrap install-to-root / && \
+rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos-lts
+rpm-ostree install kernel-cachyos-lts-devel
---
+++ config/scripts/selinux_policy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo 'Enable SElinux policy'
+setsebool -P domain_kernel_load_modules on
