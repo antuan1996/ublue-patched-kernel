5f345b15d1aa4b00bc18d5ace4cd2ddfad7fee92
antuan1996 <antuan1996@users.noreply.github.com>
2023-11-08 12:54:44 +0300

    disable uksmd for now

--- config/scripts/cachyos_kernel_override.sh
+++ config/scripts/cachyos_kernel_override.sh
@@ -11,5 +11,5 @@ rpm-ostree cliwrap install-to-root / && \
 rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos-lts --install kernel-cachyos-lts-headers    
 # rpm-ostree install kernel-cachyos-lts-devel
 
-rpm-ostree install libcap-ng-devel procps-ng-devel uksmd-lts
-systemctl enable uksmd.service
+# rpm-ostree install libcap-ng-devel procps-ng-devel uksmd-lts
+# systemctl enable uksmd.service
